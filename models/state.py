cc1ac2da89ef0b1fe0182130c3ef50f68e592488
rayd1893 <rayd1893@gmail.com>
2021-12-21 11:26:40 -0500

    Add conditional to work with DBStorage or FileStorage

--- models/state.py
+++ models/state.py
@@ -4,10 +4,15 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import Column
 from sqlalchemy.sql.sqltypes import String
 from models.base_model import BaseModel, Base
+from models import storage, type_storage
+from models.city import City
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship('City')
+    if type_storage == "db":
+        cities = relationship('City')
+    else:
+        cities = storage.all(City)
