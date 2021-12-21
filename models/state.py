bb6d110406e1c42b140282e12c01a32eeb343416
rayd1893 <rayd1893@gmail.com>
2021-12-21 00:07:35 -0500

    Add SQLAlchemy to create table states

--- models/state.py
+++ models/state.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import String
+from models.base_model import BaseModel, Base
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship('City')
