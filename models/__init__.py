ecf775cfafc14dc417937b119eff3bdd967b6b0e
rayd1893 <rayd1893@gmail.com>
2021-12-21 17:06:41 -0500

    Fix circular import

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
+
+
 from os import getenv
 
 type_storage = getenv("HBNB_TYPE_STORAGE")
 
 if type_storage == 'db':
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
--- models/state.py
+++ models/state.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import Column
 from sqlalchemy.sql.sqltypes import String
 from models.base_model import BaseModel, Base
-from models import storage, type_storage
+import models
 from models.city import City
 
 
@@ -12,7 +12,7 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    if type_storage == "db":
+    if models.type_storage == "db":
         cities = relationship('City')
     else:
-        cities = storage.all(City)
+        cities = models.storage.all(City)
