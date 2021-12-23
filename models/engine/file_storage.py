e1f97eae7d0bccd34f5abbd96c8b27aee5d96022
rayd1893 <rayd1893@gmail.com>
2021-12-22 22:03:38 -0500

    chnage postition of import class Place

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -45,9 +45,9 @@ class FileStorage:
         """Loads storage dictionary from file"""
         from models.base_model import BaseModel
         from models.user import User
-        from models.place import Place
         from models.state import State
         from models.city import City
+        from models.place import Place
         from models.amenity import Amenity
         from models.review import Review
 
