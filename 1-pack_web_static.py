178d80a274eaf10e89995b9e25e152578457a4f8
Rayd Trujillo <rayd1893@gmail.com>
2022-01-14 02:50:48 +0000

    Compress folder web_static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''Create file tgz that contents web statics'''
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    '''Function to run fabric'''
+    now = datetime.now()
+    year = str(now.year)
+    month = str(now.month).zfill(2)
+    day = str(now.day).zfill(2)
+    hour = str(now.hour).zfill(2)
+    minute = str(now.minute).zfill(2)
+    second = str(now.second).zfill(2)
+    folder = "web_static"
+    tar = "versions/" + folder + "_" + year + month + day
+    tar += hour + minute + second
+    command = "tar -cvzf "+ tar + ".tgz " + folder
+    try:
+        local("mkdir -p version")
+        local(command)
+        return tar
+    except Exception as e:
+        return None
